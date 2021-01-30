ae467b3b13c9e6cbb6d30649f9bf036f5ab5f90d
Kacper <kacperjankowiak8@gmail.com>
2021-01-30 11:40:33 +0000

    Zmiany w AppTest przez KJ

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -2,11 +2,15 @@ package com.mycompany.app;
 
 import static org.junit.Assert.assertTrue;
 
+import net.bytebuddy.implementation.bytecode.Throw;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Unit test for simple App.
@@ -17,18 +21,41 @@ public class AppTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void LogInAndLogOut()
     {
         assertTrue( true );
-        //System.setProperty("webdriver.chrome.driver","D:\\Kurs\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
 
         ChromeOptions options = new ChromeOptions();
-        options.setHeadless(true);
-
         ChromeDriver driver = new ChromeDriver(options);
-        driver.get("http://www.example.com");
-        WebElement element = driver.findElement(By.linkText("More information..."));
-        element.click();
+        //options.setHeadless(true);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        final String userName = "mojtestowyuzytkownik@test.pl";
+        final String password = "test1";
+
+        driver.get("http://automationpractice.com/");
+
+        WebElement signInButton = driver.findElement(By.linkText("Sign in"));
+        signInButton.click();
+
+
+        WebElement login_input = driver.findElement(By.xpath("//*[@id=\'email\']"));
+        login_input.sendKeys(userName);
+
+        WebElement password_input = driver.findElement(By.cssSelector("#passwd"));
+        password_input.sendKeys(password);
+
+        WebElement sign_in_button = driver.findElement(By.name("SubmitLogin"));
+        sign_in_button.click();
+
+        WebElement cart = driver.findElement(By.cssSelector("#header > div:nth-child(3) > div > div > div:nth-child(3) > div > a > b"));
+        String trueCart = "Cart";
+        Assert.assertEquals(cart.getText(), trueCart);
+        System.out.println("Test wykazał, że wskazane pole nazywa się: " + cart.getText() + " i jest zgodne z nazwą, która miała si ęwyświetlić czyli: " + trueCart);
+
+        WebElement log_out_button = driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[2]/a"));
+        log_out_button.click();
         driver.quit();
-    }
+        }
 }
