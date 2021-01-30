3aa0aa3941ac3021947d0a14c41912b97b852f4a
Katarzyna <mymrowcia@gmail.com>
2021-01-30 13:20:39 +0100

    poprawka

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -11,24 +11,40 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-        //System.setProperty("webdriver.chrome.driver","D:\\Kurs\\chromedriver.exe");
+    public void userCanLogIn() {
+        //assertTrue( true );
+        // System.setProperty("webdriver.chrome.driver", "/Applications/kurs/driver/chromedriver");
 
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(true);
 
         ChromeDriver driver = new ChromeDriver(options);
-        driver.get("http://www.example.com");
-        WebElement element = driver.findElement(By.linkText("More information..."));
-        element.click();
+
+        final String username = "mojtestowyuzytkownik@test.pl";
+        final String password = "test1";
+        driver.get("http://automationpractice.com");
+
+        WebElement signInButton = driver.findElementByLinkText("Sign in");
+        signInButton.click();
+
+        WebElement login_input = driver.findElementById("email");
+        login_input.sendKeys(username);
+
+        WebElement password_input = driver.findElementById("passwd");
+        password_input.sendKeys(password);
+
+        WebElement sign_in_button = driver.findElement(By.id("SubmitLogin"));
+        sign_in_button.click();
+
+        WebElement log_out_button = driver.findElement(By.linkText("Sign out"));
+        log_out_button.click();
+
+
         driver.quit();
     }
 }
